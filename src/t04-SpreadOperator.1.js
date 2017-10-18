2df074540b046658d83505d1f5a62c93e60e0d17
marcpre <marcus.presich@gmail.com>
2017-10-18 15:38:26 +0000

    adding rest and spread operator

---
+++ src/t04-SpreadOperator.1.js
@@ -0,0 +1,7 @@
+const colors = [ "red", "green", "blue" ]
+const fruits = [ "bananas", "apples", "peas" ]
+
+const pizzas = [...colors, " hier comes the colors ", ...fruits]
+
+console.log(pizzas)
+
---
+++ src/t05-RestOperator.js
@@ -0,0 +1,5 @@
+function volume(width, length, height) {
+    return width * length * height;
+};
+ 
+console.log(volume(...[2, 8, 5]));
