b533a906b2c466e74efce68e2ad3ed808916625b
marcpre <marcus.presich@gmail.com>
2017-10-25 11:55:27 +0000

    adding prototypes to function

---
+++ src/t08-Prototypes.js
@@ -0,0 +1,16 @@
+function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+}
+Person.prototype.nationality = "English"
+
+Person.prototype.getLastName =  function(){
+  return this.firstName
+}
+
+const testName = new Person("Hans", "Zimmer", 18, "blue")
+
+console.log(testName.nationality)
+console.log(testName.getLastName())
