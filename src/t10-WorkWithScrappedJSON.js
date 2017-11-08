6ed162e5eb74c122d9d07058301736eb2c357e7d
marcpre <marcus.presich@gmail.com>
2017-11-08 05:46:44 +0000

    creating mve

--- src/t10-WorkWithScrappedJSON.js
+++ src/t10-WorkWithScrappedJSON.js
@@ -3,4 +3,43 @@ const path = require('path')
 
 const data = JSON.parse(fs.readFileSync(path.join(__dirname, './data/allCoins.json'), 'utf8'))
 
-console.log(data.currency)
+console.log(data)
+
+const data = {
+  currency: [{},
+    {
+      Name: 'Euro',
+      Url: '/currencies/Euro/',
+      Symbol: 'EUR',
+      Price: '$76.3'
+    },
+    {
+      Name: 'Dollar',
+      Url: '/currencies/dollar/',
+      Symbol: 'DOL',
+      Price: '$27.61'
+    },
+    {
+      Name: 'Yen',
+      Url: '/currencies/yen/',
+      Symbol: 'Yen',
+      Price: '$638234.60'
+    }
+  ]
+}
+
+
+const coins = []
+data.currency.forEach((cur) => {
+  if (cur.CoinName) {
+    coins.push({
+      coinname: cur.Name,
+      symbol: cur.Symbol,
+      price: cur.Price,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    })
+  }
+})
+
+console.log(coins)
