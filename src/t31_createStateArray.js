9026e888c057093de2f382fcb87b45985a979653
marcpre <marcus.presich@gmail.com>
2019-06-22 19:33:59 +0200

    create js example

---
+++ src/t31_createStateArray.js
@@ -0,0 +1,15 @@
+let ticker = ['AAPL', 'MSFT']
+let mbeatUrlEndings = ['price-target/?MostRecent=0', 'insider-trades/', 'institutional-ownership/', 'earnings/', 'dividend/']
+
+/*
+ * {
+ * ticker: 'AAPL',
+ * url: 'price-target/?MostRecent=0',
+ * state: true // for successfully scrapped
+ * },
+ * {
+ * ticker: 'AAPL',
+ * url: 'insider-trades/',
+ * state: false // for successfully scrapped
+ * }
+ */
