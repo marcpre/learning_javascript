8e662882ae24ea59f96aea329d0ad5e8f93d71ef
marcpre <marcus.presich@gmail.com>
2019-06-23 17:14:59 +0200

    set/get state

--- src/t31_createStateArray.js
+++ src/t31_createStateArray.js
@@ -1,5 +1,48 @@
-let ticker = ['AAPL', 'MSFT']
-let mbeatUrlEndings = ['price-target/?MostRecent=0', 'insider-trades/', 'institutional-ownership/', 'earnings/', 'dividend/']
+const ticker = ['AAPL', 'MSFT']
+const mbeatUrlEndings = ['price-target/?MostRecent=0', 'insider-trades/', 'institutional-ownership/', 'earnings/', 'dividend/']
+
+
+const res = []
+for (let i = 0; i < ticker.length; i++) {
+  res.push({
+    ticker: ticker[i],
+    url: 'price-target/?MostRecent=0',
+    state: false,
+  }, {
+    ticker: ticker[i],
+    url: 'insider-trades/',
+    state: false,
+  }, {
+    ticker: ticker[i],
+    url: 'institutional-ownership/',
+    state: false,
+  }, {
+    ticker: ticker[i],
+    url: 'earnings/',
+    state: false,
+  }, {
+    ticker: ticker[i],
+    url: 'dividend/',
+    state: false,
+  })
+}
+
+console.log(JSON.stringify(res))
+
+function setState(res, state) {
+  res.state = state
+}
+
+function getState(res) {
+  return res.state
+}
+
+setState(res[1], true)
+
+console.log(getState(res[2]))
+console.log(getState(res[1]))
+
+console.log(JSON.stringify(res))
 
 /*
  * {
